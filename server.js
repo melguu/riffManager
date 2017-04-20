8648690f483c42d2980ee48c715a249d8cb41a61
milosberka <milos.berka@metropolia.fi>
2017-04-20 10:07:41 +0300

    Added model defs

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
 'use strict';
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
+mongoose.connect('mongodb://localhost/riffManager');
+
+var Riff = mongoose.model('Riff', {
+    name: String,
+    creator: String,
+    instrument: String,
+    description: String,
+    length: Int,
+    key: String,
+    speed: Int,
+    genre: String,
+    type: String
+});
+
+var SongWorkspace = mongoose.model('SongWorkspace', {
+    name: String,
+    creator: String,
+    users: Array,
+    song: String,
+    riffs: Array,
+    messages: Array,
+});
+
+var Song = mongoose.model('Song', {
+    name: String,
+    creator: String,
+    instrument: String,
+    description: String,
+    length: Int,
+    key: String,
+    speed: Int,
+    genre: String,
+    riffs: Array
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
